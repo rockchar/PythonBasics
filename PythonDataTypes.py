841a35475210bf2fb0ae6a60d5bc8ed8646c14c5
rohit <rohitsinha050481@gmail.com>
2019-03-28 14:06:05 +0530

    Create PythonDataTypes.py
    First Commit

---
+++ PythonDataTypes.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 13:55:27 2019
+
+@author: rkumar43
+"""
+
+#             Python Data Types                
+"""
+The following are the data types available in python
+
+1. Integers         - int   - whole numbers such as 200 , 300 
+2. Floating point   - float - numbers with decimal point like 3.45 
+3. Strings          - str   - immutable,ordered sequence of characters
+4. Lists            - list  - mutable ordered sequence of objects
+5. Dictionaries     - dict  - unordered key value pairs 
+6. Tuples           - tup   - Ordered immutable sequence of objects
+7. Sets             - set   - Unordered collection of unique objects
+8. Booleans         - bool  - Logical value indicating true or false
+
+"""
+
+#            Let us go through the data types 
