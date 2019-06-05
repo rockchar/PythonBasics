0dbaa921cc134d980469c638b716451db8a17ed9
Rohit <rohitk.in@gmail.com>
2019-06-05 23:03:20 +0530

    Exercises on python statements3

--- PythonStatementsExercises.py
+++ PythonStatementsExercises.py
@@ -77,6 +77,11 @@ print(my_list)
 #Use List Comprehension to create a list of the first letters of every word in 
 #the string below:
 st = 'Create a list of the first letters of every word in this string'
+
+#alternate method 
+print([word[0] for word in st.split()])
+
+#long method
 my_list = st.split()
 letter_list=[]
 for word in my_list:
