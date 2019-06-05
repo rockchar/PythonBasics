09d27d1f975a22d45bb85ab54bae9c0a1be64cce
Rohit <rohitk.in@gmail.com>
2019-06-05 10:27:20 +0530

    Exercises on list astrings sets and dictionaries 2

--- Exerises.py
+++ Exerises.py
@@ -14,6 +14,7 @@ Dictionaries:unordered pair of key value
 sets : unordered collection of unique objects 
 """
 
+#------------------------- String Slicing -------------------------------
 #s = 'hello'
 # Print out 'e' using indexing
 
@@ -38,9 +39,12 @@ print(s[4])
 # Method 2:
 print(s[-1])
 
+#----------------------------- lists -------------------------------------
+
 # Build this list [0,0,0] two separate ways.
 # Method 1:
 mylist=[]
+#using append
 mylist.append(0)
 mylist.append(0)
 mylist.append(0)
@@ -48,10 +52,32 @@ mylist.append(0)
 print(mylist)
 
 # Method 2:
+#using assignment
 mylist1 = [0,0,0]
 print(mylist1)
 
 # Method 3:
+#using concatenation
 mylist2 = []
 mylist2 = mylist2 + mylist #only a list can concatenate with a list
 print(mylist2)
+
+# Reassign 'hello' in this nested list to say 'goodbye' instead:
+list3 = [1,2,[3,4,'hello']]
+print(list3)
+
+list3[2][2] = "goodbye"
+
+print(list3)
+
+#Sort the list below
+list4 = [5,3,4,6,1]
+
+list4.sort()
+print(list4)
+
+#----------------------------------- DICTIONARIES --------------------------
+
+
+
+
