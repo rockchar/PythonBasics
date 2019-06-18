22fa95d0885590007653be96a4e681722be0d8dd
Rohit <rohitk.in@gmail.com>
2019-06-18 20:45:27 +0530

    functions 1

--- LamdaExpressionsMapAndFilter.py
+++ LamdaExpressionsMapAndFilter.py
@@ -13,3 +13,32 @@ again.
 
 
 '''
+
+'''
+map function
+The map function allows you to "map" a function to an iterable object. 
+That is to say you can quickly call the same function to every item in an 
+iterable, such as a list. For example:
+'''
+def square(num):
+    return num**2
+
+mylist=[1,2,3,4]
+for item in map(square,mylist):
+    print(item)
+    
+square_list = list(map(square,mylist))
+
+print(square_list)
+print(list(map(square,mylist)))
+
+#lets try some complex function
+def splicer(name):
+    if len(name)%2==0:
+        return True
+    else:
+        return name[0]
+    
+name_list = ["Rohit","EVAN","MEAW"]
+splice_list = list(map(splicer,name_list))
+print(splice_list)
