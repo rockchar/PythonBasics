fb1097228226977d1f24565bc7c288f5917761d1
Rohit <rohitk.in@gmail.com>
2019-06-18 20:52:26 +0530

    functions filters

--- LamdaExpressionsMapAndFilter.py
+++ LamdaExpressionsMapAndFilter.py
@@ -6,13 +6,6 @@ Created on Sun Jun 16 13:12:03 2019
 @author: rockchar
 """
 
-'''
-Lamda expression is a quick one time use function and also known as ananomys 
-function. They are one time function and used just once and never referenced 
-again.
-
-
-'''
 
 '''
 map function
@@ -20,6 +13,7 @@ The map function allows you to "map" a function to an iterable object.
 That is to say you can quickly call the same function to every item in an 
 iterable, such as a list. For example:
 '''
+
 def square(num):
     return num**2
 
@@ -42,3 +36,27 @@ def splicer(name):
 name_list = ["Rohit","EVAN","MEAW"]
 splice_list = list(map(splicer,name_list))
 print(splice_list)
+
+'''
+Filter functions:
+Filter functions can be applied on an iterable but here the method filters
+the results based on only the inputs that return true for the given condition
+
+'''
+
+#example to filter all the even numbers in a list
+
+my_list = [1,2,3,4,5,6,7,8]
+
+def myEvenfilter(num):
+    return num%2==0    # evaluates to True or False
+
+even_list = list(filter(myEvenfilter,my_list))
+print(even_list)
+
+
+'''
+Lamda expression is a quick one time use function and also known as ananomys 
+function. They are one time function and used just once and never referenced 
+again.
+'''
