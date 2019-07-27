61af39e494b68c43f47b091916218d86cd36a366
Rohit <rohitk.in@gmail.com>
2019-07-27 20:45:10 +0530

    nested statements

---
+++ nestedstatementsandscope.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 22 22:26:02 2019
+
+@author: rockchar
+"""
+def main():
+    print("hello world")
+    
+    
+if __name__== '__main__':
+    main()
+    
